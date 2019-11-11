4feed1f18ffcc0616b4d2960c2084bd513b56596
Juangustasonic <juanmcasper5@gmail.com>
2019-11-11 03:02:18 +0100

    2 jugadores con funcionalidad de saltar , menu , creditos y escena de muerte PROBLEMAS: BUG CON PLATAFORMAS CUANDO MUERE UNO DE LOS DOS JUGADORES LOCALES TODAVIA NO SE SABE COMO IMPLEMENTAR agacharse SIN CREAR UN NUEVO PLAYER TIEMPO NO AÑADIDO EN ESTA VERSIÓN PARA  POWERUPS Y SCORE FALTARÍA INDICAR QUIEN ES EL GANADOR (es decir, cuando ambos mueran, el que más haya durado se indique en la nueva escena)

---
+++ 2 jugadores local con menus/menu.js
@@ -0,0 +1,58 @@
+class menu extends Phaser.Scene {
+    constructor() {
+        super("menuPrincipal");
+    }
+
+    create (){
+        //const juego = this.add.text(20,20, "Cargando Juego...")
+        //this.scene.start("menuMuerte");
+       
+    
+        this.clickButton = this.add.text(100, 100, 'Jugar', { fill: '#0f0' })
+         .setInteractive()
+         .on('pointerdown', () =>  this.scene.start("PlayGame"))
+         .on('pointerover', () => this.enterButtonHoverState() )
+         .on('pointerout', () => this.enterButtonRestState() );
+
+         this.clickButton2 = this.add.text(300, 100, 'Créditos', { fill: '#0f0' })
+         .setInteractive()
+         .on('pointerdown', () =>  this.scene.start("Creditos"))
+         .on('pointerover', () => this.enterButtonHoverState2() )
+         .on('pointerout', () => this.enterButtonRestState2() );
+    
+        this.updateClickCountText(clickCount);
+      }
+    //update(){
+      
+    
+      enterButtonHoverState() {
+        this.clickButton.setStyle({ fill: '#ff0'});
+      }
+    
+      enterButtonRestState() {
+        this.clickButton.setStyle({ fill: '#0f0' });
+      }
+
+      enterButtonHoverState2() {
+        this.clickButton2.setStyle({ fill: '#ff0'});
+      }
+    
+      enterButtonRestState2() {
+        this.clickButton2.setStyle({ fill: '#0f0' });
+      }
+    
+    
+   // }
+
+
+
+
+
+
+
+
+
+
+
+
+}
