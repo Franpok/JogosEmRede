9b310ab3c3b8aff5936d67fd41efc2f0f2291ebf
Juangustasonic <juanmcasper5@gmail.com>
2019-11-12 20:19:08 +0100

    AÑADIDO EASTER EGG XD

--- Juego 0.4/creditos.js
+++ Juego 0.4/creditos.js
@@ -7,8 +7,16 @@ class creditos extends Phaser.Scene {
       this.load.image("back", "resources/back.png")
       this.load.image("credits", "resources/creditosPantalla.png")
       this.load.image("fondo", "resources/fondoMenu.png")
+      this.load.audio("sonido", ["resources/Never.mp3"]);
     }
     create(){
+      this.clickButton = this.add.text(600, 300, 'EASTER EGG XD',{fontSize: "50px"})
+      .setInteractive()
+      .on('pointerdown', () =>  sonidoepico.play());
+      
+      let sonidoepico = this.sound.add("sonido");
+      sonidoepico.loop = false;
+     
       var fondo = this.add.image(540,360,'fondo');
       var credits = this.add.image(540,360,'credits');
     var back = this.add.image(200, 600, 'back')
